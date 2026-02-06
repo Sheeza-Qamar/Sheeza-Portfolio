0da19cbd37f4066f637cd7c801c786a77d9a6627
Sheeza Qamar <ssheezavirk@gmail.com>
2026-02-06 23:10:04 +0500

    changed profile pic

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -18,7 +18,7 @@ export const CTA = () => {
                         rounded-full border-3 border-solid border-gray-900"
                     >
                         <Image
-                            src="/images/profile/developer-pic.png.jpeg"
+                            src="/images/profile/developer-pic.png"
                             width={70}
                             height={100}
                             alt="Sheeza"
